8a209a532bd29b3a23f81e268f5b4cea4507d832
Howard Huang <hao@hwrd.me>
2017-04-01 16:12:34 -0700

    Add some sensible defaults

--- customizations/completions.bash
+++ customizations/completions.bash
@@ -3,3 +3,12 @@ if [[ `uname` = "Darwin" ]]; then
     for file in $(brew --prefix)/etc/bash_completion.d/*; do source $file; done
   fi
 fi
+
+# Case-insensitive completions
+bind "set completion-ignore-case on"
+
+# Treat - and _ as equivalent for completions
+bind "set completion-map-case on"
+
+# Show ambiguous matches with single tab instead of double
+bind "set show-all-if-ambiguous on"
---
+++ customizations/redirection.bash
@@ -0,0 +1,3 @@
+# Prevent file overwrite on STDOUT redirection
+# Use `>|` to force redirection to existing file
+set -o noclobber
