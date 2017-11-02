4f9cb833c6af2d24233fbb1ad8d882c6c64bb98e
Howard Huang <hao@hwrd.me>
2017-11-01 18:16:13 -0700

    Change some Rg settings

--- customizations/fzf.bash
+++ customizations/fzf.bash
@@ -1,4 +1,4 @@
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
 # From: https://mkaz.tech/geek/unix-is-my-ide/
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
