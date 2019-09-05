751c852dec43c67012d2685e0286332abac9853c
Howard Huang <hao@hwrd.me>
2019-09-05 15:29:51 -0700

    Change terminal to kitty for Linux

--- os/linux.sh
+++ os/linux.sh
@@ -7,5 +7,5 @@ eval $(keychain --eval --quiet id_rsa)
 export BROWSER=firefox
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export PATH="$HOME/.local/bin:$PATH"
-export TERMINAL=alacritty
+export TERMINAL=kitty
 export ANDROID_HOME=/opt/android-sdk
