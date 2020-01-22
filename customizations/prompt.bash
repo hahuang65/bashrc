be149414a2ad065d96ff217134e997356c14b03a
Howard Huang <hao@hwrd.me>
2020-01-21 18:24:25 -0800

    Reset prompt to just a basic symbol

--- customizations/prompt.bash
+++ customizations/prompt.bash
@@ -1,9 +1,1 @@
-bash_prompt() {
-  if [ "$?" == "0" ]; then
-    echo -e '\e[0;32m$\033[0m '
-  else
-    echo -e '\e[0;31m$\033[0m '
-  fi
-}
-
-export PROMPT_COMMAND='PS1=$(bash_prompt)'
+export PS1='$ '
