abcd40eb980b22a8ac2e5a4a1a08028741b46acc
Howard Huang <h.huang65@gmail.com>
2020-03-23 11:16:39 -0700

    Use bat for more utilities if available

--- customizations/bat.bash
+++ customizations/bat.bash
@@ -1,4 +1,18 @@
 if hash bat 2> /dev/null; then
+  export BAT_PAGER="less --raw-control-chars"
   alias cat="bat"
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+  function tail {
+    command tail "$@" | bat --paging=never --language log
+  }
+
+  if hash batman 2> /dev/null; then
+    alias man="batman"
+  else
+    export MANPAGER="sh -c 'col --no-backspaces --spaces | bat --language man --plain'"
+  fi
+
+  if hash batgrep 2> /dev/null; then
+    alias rg="batgrep"
+  fi
 fi
