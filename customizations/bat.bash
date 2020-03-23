7592d68a910ab3a82e604e9df175af9a6ac53d7c
Howard Huang <h.huang65@gmail.com>
2020-03-23 11:44:13 -0700

    Don't use bat for tailing, does not seem too useful

--- customizations/bat.bash
+++ customizations/bat.bash
@@ -2,10 +2,6 @@ if hash bat 2> /dev/null; then
   export BAT_PAGER="less --raw-control-chars"
   alias cat="bat"
 
-  function tail {
-    command tail "$@" | bat --paging=never --language log
-  }
-
   if hash batman 2> /dev/null; then
     alias man="batman"
   else
