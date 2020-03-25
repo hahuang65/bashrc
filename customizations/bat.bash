64c9dfeb70c99f76cd4b607095096f680a525eae
Howard Huang <h.huang65@gmail.com>
2020-03-25 14:15:04 -0700

    Only set pager for using bat with man

--- customizations/bat.bash
+++ customizations/bat.bash
@@ -1,11 +1,10 @@
 if hash bat 2> /dev/null; then
-  export BAT_PAGER="less --raw-control-chars"
   alias cat="bat"
 
   if hash batman 2> /dev/null; then
     alias man="batman"
   else
-    export MANPAGER="sh -c 'col -bx | bat --language man --plain'"
+    export MANPAGER="sh -c 'col -bx | bat --language man --plain --pager=\"less --raw-control-chars\"'"
   fi
 
   if hash batgrep 2> /dev/null; then
