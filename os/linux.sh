5e48d450da459a889d64ff5d9e8249771f9cda96
Howard Huang <h.huang65@gmail.com>
2020-04-24 23:33:59 -0700

    Switch to Chromium

--- os/linux.sh
+++ os/linux.sh
@@ -4,8 +4,7 @@ fi
 
 eval $(keychain --eval --quiet id_rsa)
 
-export BROWSER=firefox
+export BROWSER=chromium
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export PATH="$HOME/.local/bin:$PATH"
 export TERMINAL=alacritty
-export ANDROID_HOME=/opt/android-sdk
