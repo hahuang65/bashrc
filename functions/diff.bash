1e5b7f78cd7e6681093e07a8e47d868dcefec1a1
Howard Huang <h.huang65@gmail.com>
2020-05-05 12:27:14 -0700

    Change to using delta for diffs

--- functions/diff.bash
+++ functions/diff.bash
@@ -1,4 +1,8 @@
-if hash diff-so-fancy 2> /dev/null; then
+if hash delta 2> /dev/null; then
+  function diff {
+    command diff -u "$@" | delta
+  }
+elif hash diff-so-fancy 2> /dev/null; then
   function diff {
     command diff -u "$@" | diff-so-fancy | less --tabs=4 -RFX
   }
