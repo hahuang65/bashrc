67815dabbff685e4ce8dd852829e17114572a2d1
Howard Huang <h.huang65@gmail.com>
2021-02-09 21:09:29 -0800

    Switch editor to use Emacs

--- customizations/editor.bash
+++ customizations/editor.bash
@@ -1,2 +1,9 @@
-export EDITOR="vim"
-export VISUAL="vim"
+if hash emacsclient 2> /dev/null; then
+  export ALTERNATE_EDITOR=""
+  export EDITOR="emacsclient --tty"
+  export VISUAL="emacsclient --tty --alternate-editor=emacs"
+  alias emacs="emacsclient --tty --alternate-editor=emacs"
+else
+  export EDITOR="vim"
+  export VISUAL="vim"
+fi
