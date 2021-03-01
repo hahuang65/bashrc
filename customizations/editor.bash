a867fafc96a995b970f5f15ed6a02a88ef879b39
Howard Huang <h.huang65@gmail.com>
2021-03-01 11:06:04 -0800

    Use GUI Emacs for EDITOR

--- customizations/editor.bash
+++ customizations/editor.bash
@@ -1,8 +1,8 @@
 if hash emacsclient 2> /dev/null; then
   export ALTERNATE_EDITOR=""
-  export EDITOR="emacsclient --tty"
-  export VISUAL="emacsclient --tty --alternate-editor=emacs"
-  alias emacs="emacsclient --tty --alternate-editor=emacs"
+  export EDITOR="emacsclient --create-frame"
+  export VISUAL="emacsclient --create-frame --alternate-editor=emacs"
+  alias emacs="emacsclient --create-frame --alternate-editor=emacs"
 else
   export EDITOR="vim"
   export VISUAL="vim"
