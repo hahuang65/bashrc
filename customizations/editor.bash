0ccdcf7428357224755e2bee0f2d940cb3cee229
Howard Huang <h.huang65@gmail.com>
2021-03-05 11:29:45 -0800

    Use nvim if available

--- customizations/editor.bash
+++ customizations/editor.bash
@@ -1,4 +1,8 @@
-if hash emacsclient 2> /dev/null; then
+if hash nvim 2> /dev/null; then
+  export EDITOR="nvim"
+  export VISUAL="nvim"
+  alias vim=nvim
+elif hash emacsclient 2> /dev/null; then
   export ALTERNATE_EDITOR=""
   export EDITOR="emacsclient --create-frame"
   export VISUAL="emacsclient --create-frame --alternate-editor=emacs"
