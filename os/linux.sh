134b1abaefbdcce90c5630daf8424a52999c42a7
Howard Huang <h.huang65@gmail.com>
2021-03-18 12:55:47 -0700

    Switch browser to Google Chrome, remove alacritty as TERMINAL since kitty is the default

--- os/linux.sh
+++ os/linux.sh
@@ -4,7 +4,6 @@ fi
 
 eval $(keychain --eval --quiet id_rsa)
 
-export BROWSER=chromium
+export BROWSER=google-chrome-stable
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export PATH="$HOME/.local/bin:$PATH"
-export TERMINAL=alacritty
