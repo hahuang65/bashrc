53a2a2b39b151288e9e48edd3028fa48989cf921
Howard Huang <h.huang65@gmail.com>
2021-03-22 10:38:45 -0700

    Switch to Firefox

--- os/linux.sh
+++ os/linux.sh
@@ -4,6 +4,5 @@ fi
 
 eval $(keychain --eval --quiet id_rsa)
 
-export BROWSER=google-chrome-stable
-export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export BROWSER=firefox
 export PATH="$HOME/.local/bin:$PATH"
