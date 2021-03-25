0de153767c641a42606ffb6f44258dfa67a33b5f
Howard Huang <h.huang65@gmail.com>
2021-03-25 14:09:55 -0700

    Prevent keychain from asking for SSH password until needed on Linux

--- os/linux.sh
+++ os/linux.sh
@@ -2,7 +2,7 @@ if [ -z $DISPLAY ] && [ "$(tty)" == "/dev/tty1" ]; then
   exec sway
 fi
 
-eval $(keychain --eval --quiet id_rsa)
+eval $(keychain --eval --noask --quiet id_rsa)
 
 export BROWSER=firefox
 export PATH="$HOME/.local/bin:$PATH"
