82c0516d543af8f582a64794caa1a2c31b928da2
Howard Huang <h.huang65@gmail.com>
2021-05-05 15:07:03 -0700

    Set emacs mode for vim/nvim terminals

--- customizations/editor.bash
+++ customizations/editor.bash
@@ -1,3 +1,9 @@
+if [ ! -z "$VIM" ]; then
+  # Emacs mode in vim/nvim because vi-mode in vim/nvim terminal has issues.
+  # Otherwise, .inputrc sets vi-mode.
+  set -o emacs
+fi
+
 if hash nvim 2> /dev/null; then
   export EDITOR="nvim"
   export VISUAL="nvim"
