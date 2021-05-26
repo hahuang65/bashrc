945ea5ded6d999eec292da0422d888cda07db664
Howard Huang <h.huang65@gmail.com>
2021-05-25 22:01:12 -0700

    Fix comment for installing NodeJS packages in asdf

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ ln -sf "${PWD}/bash_profile" "${HOME}/.bash_profile"
 
 # This just sets up some default Python packages to be installed with asdf-python (https://github.com/danhper/asdf-python)
 ln -sf "${PWD}/customizations/asdf-python-packages" "${HOME}/.default-python-packages"
-# This just sets up some default Python packages to be installed with asdf-python (https://github.com/asdf-vm/asdf-nodejs)
+# This just sets up some default NodeJS packages to be installed with asdf-nodejs (https://github.com/asdf-vm/asdf-nodejs)
 ln -sf "${PWD}/customizations/asdf-nodejs-packages" "${HOME}/.default-npm-packages"
