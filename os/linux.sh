3721fca43bb331697bfee863b769213eb3b57ce2
Howard Huang <h.huang65@gmail.com>
2022-01-13 14:38:34 -0800

    Add new SSH key to the keychain

--- os/linux.sh
+++ os/linux.sh
@@ -2,7 +2,7 @@ if [ -z $DISPLAY ] && [ "$(tty)" == "/dev/tty1" ]; then
   exec sway
 fi
 
-eval $(keychain --eval --noask --quiet id_rsa)
+eval $(keychain --eval --noask --quiet id_ed25519 id_rsa)
 
 alias open="xdg-open"
 
