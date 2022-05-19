e02b0bd5eab36480e269615fbbba217b588bbced
Howard Huang <hao@hwrd.me>
2022-05-18 21:33:45 -0700

    Update asdf script location

--- customizations/asdf.bash
+++ customizations/asdf.bash
@@ -1,5 +1,10 @@
+# https://github.com/asdf-vm/asdf/issues/1115#issuecomment-1100806590
+unset ASDF_DIR
+
 if test -f /opt/asdf-vm/asdf.sh; then
   . /opt/asdf-vm/asdf.sh
 elif test -f /usr/local/opt/asdf/asdf.sh; then
   . /usr/local/opt/asdf/asdf.sh
+elif test -f /usr/local/opt/asdf/libexec/asdf.sh; then
+  . /usr/local/opt/asdf/libexec/asdf.sh
 fi
