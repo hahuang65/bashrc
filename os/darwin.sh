80d8fa5d36dd6f92e551d713e5d0347508c5950e
Howard Huang <hao@hwrd.me>
2022-07-05 21:45:29 -0700

    Use 1Password for ssh agent

--- os/darwin.sh
+++ os/darwin.sh
@@ -1,2 +1,5 @@
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:$PATH:/usr/local/sbin:/usr/local/bin:$HOME/Documents/Projects/a5/toolbox:$HOME/Documents/Projects/a5/dockerfiles/scripts"
 alias restart_bt='sudo pkill bluetoothd'
+
+# Use 1Password's ssh agent
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
--- os/linux.sh
+++ os/linux.sh
@@ -2,7 +2,9 @@ if [ -z $DISPLAY ] && [ "$(tty)" == "/dev/tty1" ]; then
   exec sway
 fi
 
-eval $(keychain --eval --noask --quiet id_ed25519 id_rsa)
+# Removing, in favor of using 1Password's ssh agent
+# eval $(keychain --eval --noask --quiet id_ed25519 id_rsa)
+export SSH_AUTH_SOCK=~/.1password/agent.sock
 
 alias open="xdg-open"
 
