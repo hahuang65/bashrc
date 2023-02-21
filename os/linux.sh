5c31f1bddd3eb2255baef7cb75d0e71808382d22
Howard Huang <hao@hwrd.me>
2023-02-20 22:16:48 -0800

    Remove hyprland startup

--- os/linux.sh
+++ os/linux.sh
@@ -1,8 +1,6 @@
 if [ -z "$DISPLAY" ] && [ "$(tty)" == "/dev/tty1" ]; then
 	if command -v sway; then
-		exec "$HOME/.dotfiles/sway/launch-sway"
-	elif command -v Hyprland; then
-		exec "$HOME/.dotfiles/hyprland/launch-hyprland"
+		exec sway
 	else
 		echo "No configured WM/DMs installed"
 	fi
@@ -14,6 +12,5 @@ export SSH_AUTH_SOCK=~/.1password/agent.sock
 
 alias open="xdg-open"
 
-# export BROWSER=google-chrome-stable
 export BROWSER=google-chrome-stable
 export PATH="$HOME/.local/bin:$PATH"
