00a42b041c7789d4edc82784744b09f15bd00118
Howard Huang <hao@hwrd.me>
2023-02-26 21:29:04 -0800

    Add pkgfile command-not-found

--- os/linux.sh
+++ os/linux.sh
@@ -1,9 +1,13 @@
 if [ -z "$DISPLAY" ] && [ "$(tty)" == "/dev/tty1" ]; then
-	if command -v sway; then
+	if command -v sway >/dev/null 2>&1; then
 		exec sway
 	else
 		echo "No configured WM/DMs installed"
 	fi
+fi
+
+if command -v pkgfile >/dev/null 2>&1; then
+	source /usr/share/doc/pkgfile/command-not-found.bash
 fi
 
 # Removing, in favor of using 1Password's ssh agent
