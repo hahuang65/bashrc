540ee1ceb1470760b422d5713f6d65c0adf9aa70
Howard Huang <hao@hwrd.me>
2023-03-23 13:20:18 -0700

    Add fzf extensions

--- customizations/fzf.bash
+++ customizations/fzf.bash
@@ -1,1 +1,7 @@
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+if [ "$(uname)" = "Linux" ]; then
+	source "/usr/share/fzf/completion.bash"
+	source "/usr/share/fzf/key-bindings.bash"
+elif [ "$(uname)" = "Darwin" ]; then
+	source "/usr/local/opt/fzf/shell/completion.bash"
+	source "/usr/local/opt/fzf/shell/key-bindings.bash"
+fi
