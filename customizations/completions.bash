96360d33ae81138b03bdd71dab35f5dd7781c601
Howard Huang <hao@hwrd.me>
2023-05-05 11:05:04 -0500

    Add AWS tab completion

--- customizations/completions.bash
+++ customizations/completions.bash
@@ -6,6 +6,12 @@ elif [ "$(uname)" = "Darwin" ]; then
 	fi
 fi
 
+# AWS completion
+if hash aws_completer 2>/dev/null; then
+	complete -C "$(\which aws_completer)" aws
+	complete -C "$(\which aws_completer)" awslocal
+fi
+
 # Only if the shell is interactive
 if [[ $- == *i* ]]; then
 	# Case-insensitive completions
