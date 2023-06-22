de7980ee187df2519c3002718b5377e1d00b71b4
Howard Huang <hao@hwrd.me>
2023-06-22 09:26:32 -0700

    Fix weechat scripts on macOS

--- os/darwin.sh
+++ os/darwin.sh
@@ -3,7 +3,9 @@ export PATH="$PATH:$HOME/Documents/Projects/a5/toolbox:$HOME/Documents/Projects/
 # Homebrew paths
 export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/findutils/libexec/gnubin:/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
 
-alias restart_bt='sudo pkill bluetoothd'
-
 # Use 1Password's ssh agent
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
+# Fix weechat stuck on "downloading list of scripts"
+# https://github.com/weechat/weechat/issues/1723#issuecomment-1349115378
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
