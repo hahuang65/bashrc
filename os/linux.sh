3af76ddcb77d2d5de4cc74dfe5479c37c692fd35
Howard Huang <hao@hwrd.me>
2023-07-13 13:35:26 -0700

    Run sway with nvidia

--- os/linux.sh
+++ os/linux.sh
@@ -1,13 +1,17 @@
 if [ -z "$DISPLAY" ] && [ "$(tty)" == "/dev/tty1" ]; then
-	if command -v sway >/dev/null 2>&1; then
-		exec sway
-	else
-		echo "No configured WM/DMs installed"
-	fi
+  if command -v sway >/dev/null 2>&1; then
+    if pacman -Qi nvidia >/dev/null; then
+      exec "$HOME/.dotfiles/sway/launch-nvidia-sway"
+    else
+      exec sway
+    fi
+  else
+    echo "No configured WM/DMs installed"
+  fi
 fi
 
 if command -v pkgfile >/dev/null 2>&1; then
-	source /usr/share/doc/pkgfile/command-not-found.bash
+  source /usr/share/doc/pkgfile/command-not-found.bash
 fi
 
 # Removing, in favor of using 1Password's ssh agent
