7d2c806e2300c77d01cb835685b0771d5c92c506
Howard Huang <hao@hwrd.me>
2024-01-21 23:28:24 -0600

    Use mise shims as fallbacks for non-interactive processes

--- customizations/mise.bash
+++ customizations/mise.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 export MISE_ASDF_COMPAT=1
-eval "$(mise activate --shims bash)"
+# Add shims to the path so non-interactive processes (such as `nvim`)
+# can fallback on shims, while interactive ttys enjoy the speed bonus
+# of not using shims.
+export PATH="$HOME/.local/share/mise/shims:$PATH"
+eval "$(mise activate bash)"
