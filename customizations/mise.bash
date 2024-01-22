97a76350198e72b1028e4ed4fc8a60e70a0526e6
Howard Huang <hao@hwrd.me>
2024-01-22 10:26:28 -0600

    Let nvim handle loading shims when it needs to

--- customizations/mise.bash
+++ customizations/mise.bash
@@ -2,9 +2,5 @@
 
 if hash mise 2>/dev/null; then
   export MISE_ASDF_COMPAT=1
-  # Add shims to the path so non-interactive processes (such as `nvim`)
-  # can fallback on shims, while interactive ttys enjoy the speed bonus
-  # of not using shims.
-  export PATH="$HOME/.local/share/mise/shims:$PATH"
   eval "$(mise activate bash)"
 fi
