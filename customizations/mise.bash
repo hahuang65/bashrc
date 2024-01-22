6607e691a207002cc3604d0008177548ea027874
Howard Huang <hao@hwrd.me>
2024-01-22 10:22:09 -0600

    Only load mise if it's installed

--- customizations/mise.bash
+++ customizations/mise.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-export MISE_ASDF_COMPAT=1
-# Add shims to the path so non-interactive processes (such as `nvim`)
-# can fallback on shims, while interactive ttys enjoy the speed bonus
-# of not using shims.
-export PATH="$HOME/.local/share/mise/shims:$PATH"
-eval "$(mise activate bash)"
+if hash mise 2>/dev/null; then
+  export MISE_ASDF_COMPAT=1
+  # Add shims to the path so non-interactive processes (such as `nvim`)
+  # can fallback on shims, while interactive ttys enjoy the speed bonus
+  # of not using shims.
+  export PATH="$HOME/.local/share/mise/shims:$PATH"
+  eval "$(mise activate bash)"
+fi
