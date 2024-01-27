4d17f24baafcfd2f62a936b3bb3ec360949debea
Howard Huang <hao@hwrd.me>
2024-01-26 20:22:36 -0600

    Add some improvements to fzf

--- customizations/fzf.bash
+++ customizations/fzf.bash
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+export FZF_CTRL_T_OPTS="
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --color header:italic"
+
+export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
+
 if [ "$(uname)" = "Linux" ]; then
   source "/usr/share/fzf/completion.bash"
   source "/usr/share/fzf/key-bindings.bash"
