0eb7febcb4bf31cfbeb10ff02bd7a1175815738c
Howard Huang <hao@hwrd.me>
2024-01-28 10:35:23 -0600

    Only use asdf-compatible mise on macOS (for work)

--- customizations/mise.bash
+++ customizations/mise.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if hash mise 2>/dev/null; then
-  export MISE_ASDF_COMPAT=1
+  if [ "$(uname)" = "Darwin" ]; then
+    export MISE_ASDF_COMPAT=1
+  fi
+
   eval "$(mise activate bash)"
 fi
