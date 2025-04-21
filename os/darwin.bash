a81a36d01b0e0501bdb081bb975f29589982a44e
Howard Huang <hao@hwrd.me>
2025-04-21 10:51:46 -0500

    Remove podman for macOS and just use Docker

--- os/darwin.bash
+++ os/darwin.bash
@@ -12,11 +12,3 @@ export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agen
 # Fix weechat stuck on "downloading list of scripts"
 # https://github.com/weechat/weechat/issues/1723#issuecomment-1349115378
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
-if hash podman 2>/dev/null; then
-  docker() {
-    podman "$@"
-  }
-
-  export -f docker
-fi
