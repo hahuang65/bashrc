76d4ebf7294bf69eee0769497d3b6f985c2ddb4a
Howard Huang <hao@hwrd.me>
2025-07-17 21:50:40 -0500

    Use starship if available

--- customizations/prompt.bash
+++ customizations/prompt.bash
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
-# Colors
-blue="\[\e[0;34m\]"
-green="\[\e[1;32m\]"
-red="\[\e[1;31m\]"
-reset="\[\e[m\]"
-
-prompt_command() {
-  local status="$?"
-  local status_color=""
-  if [ $status != 0 ]; then
-    status_color=$red
+_setup_prompt() {
+  if exists starship; then
+    eval "$(starship init bash)"
   else
-    status_color=$green
+    # Colors
+    blue="\[\e[0;34m\]"
+    green="\[\e[1;32m\]"
+    red="\[\e[1;31m\]"
+    reset="\[\e[m\]"
+
+    prompt_command() {
+      local status="$?"
+      local status_color=""
+      if [ $status != 0 ]; then
+        status_color=$red
+      else
+        status_color=$green
+      fi
+      PS1="${blue}$(git prompt 2>/dev/null)${reset}${status_color}\$${reset} "
+      export PS1
+    }
+
+    export PROMPT_COMMAND="${PROMPT_COMMAND}prompt_command"
   fi
-  PS1="${blue}$(git prompt 2>/dev/null)${reset}${status_color}\$${reset} "
-  export PS1
 }
 
-export PROMPT_COMMAND="${PROMPT_COMMAND}prompt_command"
+_setup_prompt
+unset -f _setup_prompt
